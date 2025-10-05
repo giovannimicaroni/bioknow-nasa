b8b3cec045682fb0dbc7552607e2d25a296995c7
Jabs Rafael <jabsr@usp.br>
2025-10-05 13:13:47 -0300

    jabs

--- biograph_3/app.py
+++ biograph_3/app.py
@@ -317,5 +317,7 @@ def heatmap_page():
     return render_template('heatmap.html', heatmap_div=heatmap_div)
 
 if __name__ == '__main__':
+    import os
     create_graph("")
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
